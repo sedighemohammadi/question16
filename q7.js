0222fc63eabdae6b579018e911b6f142df5b8226
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-25 00:47:35 +0330

    7

---
+++ q7.js
@@ -0,0 +1,16 @@
+// first you must have a good vision of "this" document to answer the questions
+
+// *****************************Question 1***********************************
+
+// Todo:complete the code to print "Hey, mom just called." in the console
+// Note: you must use "this" keyword in your code
+
+const call = {
+    caller: "mom",
+    says: function() {
+      console.log(`Hey,${this.caller} just Called.`);
+      
+    }
+  };
+  call.says(); //>>>>> Hey, mom just called.
+  
